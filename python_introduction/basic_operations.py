9506a455b971126760f09bc0e7bf9ad8f1e77875
raykiwara <raykiwara10@gmail.com>
2025-09-11 23:29:17 +0300

    basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+#script that performs addition, subtraction and multiplication on two numbers
+
+number1 = 10
+number2 = 5
+
+print("Addition of {:d} and {:d} is {:d}".format(number1, number2, (number1 + number2)))
+print("Subtraction of {:d} and {:d} is {:d}".format(number1, number2, (number1 - number2)))
+print("Multiplication of {:d} and {:d} is {:d}".format(number1, number2, (number1 * number2)))
