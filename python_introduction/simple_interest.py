59fdb9ad20ad0b48e3d0fa610094593844eb3266
raykiwara <raykiwara10@gmail.com>
2025-09-11 23:37:31 +0300

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#script that calculates the simple interest earned on an investment over a period of time
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is {interest}")
