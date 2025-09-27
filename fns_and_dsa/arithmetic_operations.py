5379e576dc5d4fbf4aba0a024cb6228ff7306f03
raykiwara <raykiwara10@gmail.com>
2025-09-27 22:12:20 +0300

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+#python script that performs basic arithmetic operations
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("Cannot divide by zero")
+            else:
+                return num1 / num2
